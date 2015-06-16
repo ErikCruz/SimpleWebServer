3acea2cb7cdc81c5806c972864c5bbd4c652c1fd
Erik Cruz <erikc1989@live.com>
2015-06-16 04:37:32 +0000

    Implementing serving static files

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT, process.env.IP);
-console.log('Server running at ' + process.env.IP+':'+process.env.PORT);
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+
+var port = process.env.PORT || 3000;
+
+var mimeTypes = {
+    'html': 'text/html',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpg',
+    'png': 'image/png',
+    'js': 'text/javascript',
+    'css': 'text/css'
+};
+
+http.createServer(function(req, res){
+   var uri = url.parse(req.url).pathname;
+   var filename = path.join(process.cwd(),unescape(uri));
+   console.log('loading ' + uri);
+   var stats;
+   
+   try{
+       stats = fs.lstatSync(filename);
+   } catch(e) { 
+       res.writeHead(404, {'Content-type': 'text/plain'});
+       res.write('404 not found');
+       res.end();
+       return;
+   }
+   
+   if(stats.isFile()){
+       var mimType = mimeTypes[path.extname(filename).split('.').reverse()[0]];
+       res.writeHead(200, {'Content-type': mimType});
+       var fileStream = fs.createReadStream(filename);
+       fileStream.pipe(res);
+   } else if(stats.isDirectory()) {
+       res.writeHead(302, {
+           'Location': 'index.html'
+       });
+       res.end();
+   } else {
+       res.writeHead(500, {'Content-type': 'text/plain'});
+       res.write('500 Internal Error');
+       res.end();
+   }
+   
+}).listen(process.env.PORT);
